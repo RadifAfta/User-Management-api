f18f826ecc79ef6cb05bb7c6f8b3ab039c54071e
RadifAfta <trydi2611@gmail.com>
2025-03-12 12:55:10 +0700

    fix auth credintial missing

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/', [AuthController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
